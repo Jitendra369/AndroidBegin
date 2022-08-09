a82081fca65df0a2041f9d02115195838e6ff052
Jitendra Kadu <onekb.twotb@gmail.com>
2022-08-09 19:16:07 +0530

    version 2.0

--- app/src/main/java/com/example/myapplication/ActivitySuperUser.java
+++ app/src/main/java/com/example/myapplication/ActivitySuperUser.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ActivitySuperUser extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class ActivitySuperUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_super_user);
+        Toast.makeText(getApplicationContext(),"custome text",Toast.LENGTH_SHORT).show();
     }
 }
